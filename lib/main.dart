64f1861916be4bedc4ade7bba7601fbe00a87014
AnujPandey007 <panuj330@gmail.com>
2023-08-14 18:10:45 +0530

    Api test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tests/counter.dart';
-
+import 'package:flutter_tests/user_model.dart';
+import 'package:flutter_tests/user_repository.dart';
+import 'package:http/http.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -13,8 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -31,31 +31,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Counter counter = Counter();
-
-  void _incrementCounter() {
-    setState(() {
-      counter.incrementCount();
-    });
-  }
+  Future<User> getUsers = UserRepository(Client()).getUser();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Text(
-          '${counter.getCount()}',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: FutureBuilder(
+        future: getUsers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Center(
+            child: Text(
+              '${snapshot.data}',
+            ),
+          );
+        },
       ),
     );
   }
